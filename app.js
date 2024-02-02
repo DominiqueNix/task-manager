0b0091da01ed292e1299205896bc470096fea47f
DominiqueNix <dominiquenix@google.com>
2024-02-02 11:45:19 -0600

    update port variable

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const projectRoutes = require('./routes/projectRoute')
 const homeRoutes = require('./routes/homeRoute');
 
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())
